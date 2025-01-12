e6aff8822721006c659c67e90398b883200daa25
Kanhaiyalal Yadav <kanhaiya13004@gmail.com>
2025-01-12 12:50:57 +0530

    added session successfully

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const connection = require("./db");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+const mysql = require("mysql2");
+
 const doctorsRoutes = require("./routes/doctors"); // Doctors CRUD routes
 const hospitalsRoutes = require("./routes/hospitals");
 const patientRoutes = require("./routes/patients");
@@ -10,11 +15,26 @@ const login = require("./routes/login");
 const cors = require("cors");
 const usersRoutes = require("./routes/users");
 const receptionistRoutes = require("./routes/receptionists");
+const renderInfo = require("./routes/renderInfo");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json()); // Parse JSON bodies
 app.use(cors());
+app.use(cookieParser());
 
+const sessionStore = new MySQLStore({}, connection);
+
+app.use(
+  session({
+    key: "sessions",
+    secret: "kanhaiyaaa",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
 // Use individual routes
 app.use("/login", login);
 app.use("/doctors", doctorsRoutes);
@@ -26,6 +46,7 @@ app.use("/feedback", feedbackRoutes);
 app.use("/billing", billingRoutes);
 app.use("/users", usersRoutes);
 app.use("/receptionists", receptionistRoutes);
+app.use("/renderInfo", renderInfo);
 
 // Start the server
 const PORT = 3000;
